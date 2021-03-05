e7b1a6b08136cec95c5bee178ca787fd07ced2f5
Leon <hualiang.tian@gmail.com>
2021-03-05 15:40:50 -0500

    commit tree aa38619bf0a87995197d8517324169709496f86c

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require "pry"
 
+def my_collect(array)
+
+    i = 0
+    new_collection = [] 
+
+    while i < array.size 
+        new_collection <<  yield(array[i])
+        i += 1
+    end
+    new_collection
+
+end
 
